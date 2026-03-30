22e4002f6ca5857abf00c73a5188d1156b2a2cfe
Aditi <aditisingh100406@gmail.com>
2026-03-30 17:43:07 +0000

    learned input ,output , variables,indexing,datatypes,strings

---
+++ 03_inputvalues.py
@@ -0,0 +1,41 @@
+#a = int(input("Enter number 1: ")) 
+#b = int(input("Enter number 2: ")) 
+
+#c = print("sum of number is : ", a + b ) input() is used to take input from the user, int() is used to convert the input to an integer
+
+
+#PRACTICE SEET 2 
+
+# WAP to find remainder of a number when divided by another number
+
+#a = int(input("Enter number 1: "))
+#b = int(input("Enter number 2: "))
+
+#c = print ("Remainder of number is : ", a % b) # % is used to find the remainder of a number when divided by another number
+
+
+
+#WAP to check type of variable using input() function
+
+#a = (input(" entre a value : "))
+#print (type(a)) # type() is used to find the data type of a variable, input() function always returns a string, so the data type of a will be <class 'str'>
+
+
+#use comparision operator to find if a > b 
+
+a= int(input("Enter number 1: "))
+b = int(input("Enter number 2: "))
+print(a>b)
+
+
+# find avg of 2 numbers by taking input from users
+
+a= int(input("Enter number 1: "))
+b = int(input("Enter number 2: "))
+c = print("Average of 2 numbers is : ", (a+b)/2) # / is used to find the average of 2 numbers
+  
+
+  # find square of a number by taking input from user
+
+  a = int(input("Enter number 1: "))
+  b = print(" square of number is :" , a**2)
