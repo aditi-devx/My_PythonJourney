0b7cce9b53dd211389bf5009c3f0016d4e309e24
Aditi <aditisingh100406@gmail.com>
2026-04-04 17:50:11 +0000

    if else elif ladder

---
+++ 11_if_else_elif_ladder.py
@@ -0,0 +1,7 @@
+age = int(input("Enter a number: "   ))
+if(age> 18):
+    print("you can vote")
+elif(age == 18):\
+    print("you can vote but you are a minor")
+else:
+    print("you cannot vote")
